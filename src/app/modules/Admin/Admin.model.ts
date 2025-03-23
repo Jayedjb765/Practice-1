e5cafb3ae32b99e9fbe928cd12a06cd0e900aab1
Jayedjb765 <jayedjb76@gmail.com>
2025-03-23 13:53:29 +0600

    updating admin model

--- src/app/modules/Admin/Admin.model.ts
+++ src/app/modules/Admin/Admin.model.ts
@@ -1,6 +1,6 @@
-import { Schema } from 'mongoose';
-import { TAdmin, TUserName } from './Admin.interface';
-import { Gender } from './Admin.constant';
+import { model, Schema } from 'mongoose';
+import { AdminModel, TAdmin, TUserName } from './Admin.interface';
+import { BloodGroup, Gender } from './Admin.constant';
 
 const userNameSchema = new Schema<TUserName>({
   firstName: {
@@ -21,41 +21,81 @@ const userNameSchema = new Schema<TUserName>({
   },
 });
 
-const adminSchema = new Schema<TAdmin>({
-  id: {
-    type: String,
-    required: [true, 'Id is required'],
-    unique: true,
-  },
-  user: {
-    type: Schema.ObjectId,
-    required: [true, 'User is required'],
-    unique: true,
-    ref: 'User',
-  },
-  designation: {
-    type: String,
-    required: [true, 'Designation is required'],
-  },
-  name: {
-    type: userNameSchema,
-    required: [true, 'Name is required'],
-  },
-  gender: {
-    type: String,
-    enum: {
-      values: Gender,
-      message: `{VALUE} is not gender`,
+const adminSchema = new Schema<TAdmin, AdminModel>(
+  {
+    id: {
+      type: String,
+      required: [true, 'Id is required'],
+      unique: true,
     },
-    required: [true, 'Gender is required'],
-  },
-  dateOfBirth: {
-    type: Date,
-  },
+    user: {
+      type: Schema.ObjectId,
+      required: [true, 'User is required'],
+      unique: true,
+      ref: 'User',
+    },
+    designation: {
+      type: String,
+      required: [true, 'Designation is required'],
+    },
+    name: {
+      type: userNameSchema,
+      required: [true, 'Name is required'],
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: Gender,
+        message: `{VALUE} is not gender`,
+      },
+      required: [true, 'Gender is required'],
+    },
+    dateOfBirth: {
+      type: Date,
+    },
 
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    contactNo: {
+      type: String,
+      required: [true, 'Contact No is required'],
+    },
+    emergencyContactNo: {
+      type: String,
+      required: [true, 'Emergency Contact No is required'],
+    },
+    bloogGroup: {
+      type: String,
+      enum: {
+        values: BloodGroup,
+        message: `{VALUE} is not BloodGroup`,
+      },
+      required: [true, 'Blood Group is required'],
+    },
+    presentAddress: {
+      type: String,
+      required: [true, 'Present Address is required'],
+    },
+    permanentAddress: {
+      type: String,
+      required: [true, 'Permanent Address is required'],
+    },
+    profileImg: {
+      type: String,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+export const Admin = model<TAdmin, AdminModel>('Admin', adminSchema);
