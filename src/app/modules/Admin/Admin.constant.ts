36379bdecd2de247a910080a5e6ca3f9f3d3c142
Jayedjb765 <jayedjb76@gmail.com>
2025-03-23 13:31:38 +0600

    adding admin constant for admin in seperateFile

---
+++ src/app/modules/Admin/Admin.constant.ts
@@ -0,0 +1,13 @@
+import { TBloodGroup, TGender } from './Admin.interface';
+
+export const Gender: TGender[] = ['male', 'female', 'other'];
+export const BloodGroup: TBloodGroup[] = [
+  'A+',
+  'A-',
+  'B+',
+  'B-',
+  'AB+',
+  'AB-',
+  'O+',
+  'O-',
+];
--- src/app/modules/Admin/Admin.interface.ts
+++ src/app/modules/Admin/Admin.interface.ts
@@ -35,5 +35,6 @@ export type TAdmin = {
 };
 
 export interface AdminModel extends Model<TAdmin> {
+  // eslint-disable-next-line no-unused-vars
   isUserExist(id: string): Promise<TAdmin | null>;
 }
